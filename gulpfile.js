0aff9b9ca0b4bb2ca313a0add781e682791a2578
Kethyllen Deliperi <kethyllenldeliperi@gmail.com>
2024-01-16 15:40:44 -0300

    first commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,6 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const imagemin = require('gulp-imagemin');
-const uglify = require('gulp-uglify');
-
-function scripts() {
-    return gulp.src('./src/scripts/*.js')
-    .pipe(uglify())
-    .pipe(gulp.dest('./dist/js'))
-}
 
 function styles() {
     return gulp.src('./src/styles/*.scss')
@@ -15,15 +8,14 @@ function styles() {
         .pipe(gulp.dest('./dist/css'));
 }
 
-function images() {
-    return gulp.src('./src/images/**/*')
+function imagens() {
+    return gulp.src('./src/imagens/**/*')
         .pipe(imagemin())
-        .pipe(gulp.dest('./dist/images'));
+        .pipe(gulp.dest('./dist/imagens'));
 }
 
-exports.default = gulp.parallel(styles, images, scripts);
+exports.default = gulp.parallel(styles, imagens);
 
 exports.watch = function () {
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
-    gulp.watch('./src/scripts/*.js', gulp.parallel(scripts))
 }
